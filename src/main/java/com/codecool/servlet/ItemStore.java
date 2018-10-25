33a37d5410393f33f70b6b01b097264b7a59a312
Kovacs Reka Katalin <kovacs.reka.katalin@gmail.com>
2018-10-25 16:56:24 +0200

    itemStore gets add and remove methods and static array list

--- src/main/java/com/codecool/servlet/ItemStore.java
+++ src/main/java/com/codecool/servlet/ItemStore.java
@@ -1,4 +1,16 @@
 package com.codecool.servlet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemStore {
+    private static List<Item> listOfItems = new ArrayList<>();
+
+    public void addItem(Item item) {
+        listOfItems.add(item);
+    }
+
+    public void removeItem(Item item) {
+        listOfItems.remove(item);
+    }
 }
