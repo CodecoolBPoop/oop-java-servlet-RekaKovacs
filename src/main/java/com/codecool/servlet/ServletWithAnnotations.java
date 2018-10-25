c17cc81e275aa2ef0590262826e5400d1ae866ea
Kovacs Reka Katalin <kovacs.reka.katalin@gmail.com>
2018-10-25 16:39:01 +0200

    index page new title and check shopping cart button

--- src/main/java/com/codecool/servlet/ServletWithAnnotations.java
+++ src/main/java/com/codecool/servlet/ServletWithAnnotations.java
@@ -15,19 +15,33 @@ public class ServletWithAnnotations extends HttpServlet {
             throws IOException {
 
         PrintWriter out = response.getWriter();
-        String title = "GET method with parameters to display";
+        String title = "Welcome to the webshop ";
+
+        StringBuffer buffer = new StringBuffer();
+
 
         out.println(
                 "<html>\n" +
-                "<head><title>" + title + "</title></head>\n" +
-                "<body>\n" +
-                "<h1 align = \"center\">" + title + "</h1>\n" +
-                "<ul>\n" +
-                "<li><b>First Name</b>: " + request.getParameter("first_name") + "\n" +
-                "<li><b>Last Name</b>: " + request.getParameter("last_name") + "\n" +
-                "</ul>\n" +
-                "<div>Visit another servlet: <a href=\"/another\">Visit the other servlet</a></div>" +
-                "</body></html>"
+                        "<head><title>" + title + "</title></head>\n" +
+                        "<body>\n" +
+                        "<h1 align = \"center\">"  + title +  "</h1>\n" +
+                        "<div>" + buffer.toString() + "</div>" +
+                        "<div> <a href=\"/shopping-cart\"><button>Check Shopping Cart</button></a></div>" +
+                        "</body></html>"
         );
+
+//        out.println(
+//                "<html>\n" +
+//                "<head><title>" + title + "</title></head>\n" +
+//                "<body>\n" +
+//                "<h1 align = \"center\">"  + title +  "</h1>\n" +
+//                "<h2 align = \"center\"> Hello Baby </h1>\n" +
+//                "<ul>\n" +
+//                "<li><b>First Name</b>: " + request.getParameter("first_name") + "\n" +
+//                "<li><b>Last Name</b>: " + request.getParameter("last_name") + "\n" +
+//                "</ul>\n" +
+//                "<div>Visit another servlet: <a href=\"/another\">Visit the other servlet</a></div>" +
+//                "</body></html>"
+//        );
     }
 }
